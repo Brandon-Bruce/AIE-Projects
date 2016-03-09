5a21ecaebe192472dbfdaf66116d60e85ffca08e
Brandon Bruce <brandon.bruce@ad.aie.edu>
2016-03-09 11:37:28 +1100

    Moving to different project.

---
+++ OpenGL/OpenGL/header/GameObjects/GameObject.h
@@ -0,0 +1,17 @@
+#ifndef GAMEOBJECT_H
+#define GAMEOBJECT_H
+
+class GameObject
+{
+public:
+	void Create();
+	void Destroy();
+
+	void Update();
+	void Render();
+
+private:
+
+};
+
+#endif
---
+++ OpenGL/OpenGL/source/GameObjects/GameObject.cpp
@@ -0,0 +1,21 @@
+#include "GameObject.h"
+
+void GameObject::Create()
+{
+
+}
+
+void GameObject::Destroy()
+{
+
+}
+
+void GameObject::Update()
+{
+
+}
+
+void GameObject::Render()
+{
+
+}
